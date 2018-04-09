a31504e3a1056a6d471ee5fc106694118a2fd1cd
yenarue <yenarue@gmail.com>
2018-04-10 00:54:25 +0900

    테스트코드 임시 주석 추가

---
+++ test/unit/specs/IndentControl.spec.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld'
+
+describe('IndentControl.vue', () => {
+  it('아직 아무런 테스트가 없다', () => {
+    // 추후 추가 예정
+    // const Constructor = Vue.extend(HelloWorld)
+    // const vm = new Constructor().$mount()
+    // expect(vm.$el.querySelector('.hello h1').textContent)
+    //   .toEqual('Welcome to Your Vue.js App')
+  })
+})
